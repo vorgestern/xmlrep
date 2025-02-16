acbc98ca48c20b5973933be79add1302e80cc7d3
vorgestern <vorgestern@niemals>
2025-02-16 01:00:09 +0100

    Improved examples/gtkui

--- examples/gtkui/coverdialog.lua
+++ examples/gtkui/coverdialog.lua
@@ -1,51 +1,41 @@
 
-X=require "examples.gtkui"
+require "examples.gtkui" :makeglobal()
 
 -- https://github.com/nullobsi/cantata/blob/main/gui/coverdialog.ui
 
-local K=X.ui {
-    X.class "CoverDialog",
-    X.widget("QWidget", "CoverDialog") {
-        X.property "geometry" {
-            X.rect(0,0,417,304)
-        }
+local K=ui {
+    class "CoverDialog",
+    widget("QWidget", "CoverDialog") {
+        rectproperty("geometry", 0,0,417,304)
     },
-    X.layout ("QGridLayout", "gridLayout") {
-        X.property "margin" {
-            X.number (0)
+    layout ("QGridLayout", "gridLayout") {
+        numberproperty("margin", 0),
+        item(0,0) {
+            widget("LineEdit", "query"){}
         },
-        X.item(0,0) {
-            X.widget("LineEdit", "query"){}
-        },
-        X.item(0,1) {
-            X.widget("QPushButton", "search") {
-                X.property "text" {
-                    X.string "Search"
-                }
+        item(0,1) {
+            widget("QPushButton", "search") {
+                stringproperty("text", "Search")
             }
         },
-        X.item(0,2) {
-            X.widget("FlatToolButton", "addFileButton") {
-                X.property "tooltip" {
-                    X.string "A local file"
-                }
+        item(0,2) {
+            widget("FlatToolButton", "addFileButton") {
+                stringproperty("tooltip", "A local file")
             }
         },
-        X.item(1,0,4) {
-            X.widget("QListWidget", "list") {}
+        item(1,0,4) {
+            widget("QListWidget", "list") {}
         },
-        X.item(2,0,4) {
-            X.widget("NoteLabel", "note") {
-                X.property "text" {
-                    X.string "This can only be used to change the file used for covers, it will not alter any embedded covers you may have in your song files."
-                }
+        item(2,0,4) {
+            widget("NoteLabel", "note") {
+                stringproperty("text", "This can only be used to change the file used for covers, it will not alter any embedded covers you may have in your song files."),
             }
         },
     },
-    X.customwidgets {
-        X.customwidget {X.class "FlatToolButton", X.extends "QToolButton", X.header "support/flattoolbutton.h"},
-        X.customwidget {X.class "LineEdit", X.extends "QLineEdit", X.header "support/lineedit.h"},
-        X.customwidget {X.class "NoteLabel", X.extends "QLabel", X.header "widgets/notelabel.h"},
+    customwidgets {
+        extendedwidget("FlatToolButton", "QToolButton", "support/flattoolbutton.h"),
+        extendedwidget("LineEdit", "QLineEdit", "support/lineedit.h"),
+        extendedwidget("NoteLabel", "QLabel", "widgets/notelabel.h"),
     }
 }
 
